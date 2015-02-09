aec503004b515e789c714428b844842035f829be
yongsung7 <yongsung7@gmail.com>
2015-02-09 12:04:34 -0600

    Delete IpaExport.sh

--- IpaExport.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# original source from http://www.thecave.com/2014/09/16/using-xcodebuild-to-export-a-ipa-from-an-archive/
-xcodebuild -scheme CrowdFound -workspace Crowdfound.xcworkspace clean archive -archivePath build/CrowdFound
-xcodebuild -exportArchive -exportFormat ipa -archivePath "build/CrowdFound.xcarchive" -exportPath "build/CrowdFoundv2.ipa" -exportProvisioningProfile "Delta"
-
